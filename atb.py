be64bb17d35be531bcd9f5b8dda6e93f42b98fd6
Martin Løland <martinloland@gmail.com>
2017-08-25 19:26:43 +0200

    la til buss

--- atb.py
+++ atb.py
@@ -9,11 +9,13 @@ if r.status_code is 200:
 	soup = soup(r.content, 'html.parser')
 	planlagte_tider = soup.findAll('span', {'class':'tm-rf-planlagt'})[:3]
 	nye_tider = soup.findAll('span', {'class':'tm-rf-nytid'})[:3]
+	busser = soup.findAll('span', {'class':'tm-det-linenr'})[:3]
 
-	print('Odenseveien, mot sentrum\n')
-	print('{:10}{:10}\n{}'.format('Avgang', 'Ny tid','-'*20))
+	print('\nOdenseveien, mot sentrum\n')
+	print('{:10}{:10}{:10}\n{}'.format('Avgang', 'Ny tid','Buss','-'*25))
 
-	for plan, ny in zip(planlagte_tider, nye_tider):
-		print('{:10}{:10}'.format(plan.text, ny.text))
+	for plan, ny, buss in zip(planlagte_tider, nye_tider, busser):
+		print('{:10}{:10}{:10}'.format(plan.text, ny.text, buss.text))
+	print('\n')
 else:
 	raise UserWarning('Error making request, status code: {}'.format(r.status_code))
