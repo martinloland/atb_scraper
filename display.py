dfd7c8349ae5eeab3dc1a8ccacb86affe43eac4b
Martin Løland <martinloland@gmail.com>
2017-10-16 17:17:10 +0200

    updated pin layout

--- display.py
+++ display.py
@@ -21,22 +21,22 @@ def get_text_w16(rows):
 #     time.sleep(30)
 
 
-# # Raspberry Pi pin configuration:
-# lcd_rs        = 27  # Note this might need to be changed to 21 for older revision Pi's.
-# lcd_en        = 22
-# lcd_d4        = 25
-# lcd_d5        = 24
-# lcd_d6        = 23
-# lcd_d7        = 18
-# lcd_backlight = 4
-#
-# # Define LCD column and row size for 16x2 LCD.
-# lcd_columns = 16
-# lcd_rows    = 2
-#
-# # Initialize the LCD using the pins above.
-# lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
-#                            lcd_columns, lcd_rows, lcd_backlight)
-#
-# # Print a two line message
-# lcd.message('Hello\nworld!')
+# Raspberry Pi pin configuration:
+lcd_rs        = 26
+lcd_en        = 19
+lcd_d4        = 13
+lcd_d5        = 6
+lcd_d6        = 5
+lcd_d7        = 11
+lcd_backlight = 15
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows    = 2
+
+# Initialize the LCD using the pins above.
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+
+# Print a two line message
+lcd.message('Hello\nworld!')
